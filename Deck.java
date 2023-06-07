dd2fd54a0c43eb03ab15da584f18bcd1e3c215df
Jack Albrecht <124408265+JackAlbrecht@users.noreply.github.com>
2023-06-06 20:33:13 -0700

    Notes

--- Deck.java
+++ Deck.java
@@ -1,28 +1,29 @@
 java.util.*;
 
 class Deck {
-    private Stack<Card> cards;
+    private Stack<Card> cards; // Stores the cards in the deck
 
     public Deck() {
-        cards = new Stack<>();
-        String[] suits = {"Spades", "Hearts", "Diamonds", "Clubs"};
-        String[] ranks = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+        cards = new Stack<>(); // Initialize the stack to store the cards
+        String[] suits = {"Spades", "Hearts", "Diamonds", "Clubs"}; // Array of suits in a deck
+        String[] ranks = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}; // Array of ranks in a deck
 
+        // Create a card for each combination of suit and rank and add it to the stack
         for (String suit : suits) {
             for (String rank : ranks) {
-                Card card = new Card(suit, rank);
-                cards.push(card);
+                Card card = new Card(suit, rank); // Create a new card with the given suit and rank
+                cards.push(card); // Add the card to the stack
             }
         }
 
-        shuffle();
+        shuffle(); // Shuffle the cards in the deck
     }
 
     private void shuffle() {
-        Collections.shuffle(cards);
+        Collections.shuffle(cards); // Shuffle the cards in the deck using the Collections class
     }
 
     public Card drawCard() {
-        return cards.pop();
+        return cards.pop(); // Remove and return the top card from the stack
     }
 }
