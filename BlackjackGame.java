a3e058fbc1014f25a9f154bf0cb14e35a6d63f7d
Jack Albrecht <124408265+JackAlbrecht@users.noreply.github.com>
2023-06-06 20:29:38 -0700

    Update BlackjackGame.java

--- BlackjackGame.java
+++ BlackjackGame.java
@@ -1,58 +1,58 @@
-import java.util.*
+import java.util.*;
 
 public class BlackjackGame {
     public static void main(String[] args) {
-        int wins = 0;
-        int losses = 0;
-        boolean playAgain = true;
+        int wins = 0; // Tracks the number of wins
+        int losses = 0; // Tracks the number of losses
+        boolean playAgain = true; // Controls if the player wants to play again
 
         while (playAgain) {
-            Deck deck = new Deck();
-            Hand playerHand = new Hand();
-            Hand dealerHand = new Hand();
+            Deck deck = new Deck(); // Create a new deck of cards
+            Hand playerHand = new Hand(); // Create an empty hand for the player
+            Hand dealerHand = new Hand(); // Create an empty hand for the dealer
 
             // Deal initial cards
-            playerHand.addCard(deck.drawCard());
-            dealerHand.addCard(deck.drawCard());
-            playerHand.addCard(deck.drawCard());
-            dealerHand.addCard(deck.drawCard());
+            playerHand.addCard(deck.drawCard()); // Draw a card for the player
+            dealerHand.addCard(deck.drawCard()); // Draw a card for the dealer
+            playerHand.addCard(deck.drawCard()); // Draw a second card for the player
+            dealerHand.addCard(deck.drawCard()); // Draw a second card for the dealer
 
-            System.out.println("Player's Hand:");
+            System.out.println("Player's Hand:"); // Display the player's hand
             playerHand.display();
-            System.out.println("Player's Hand Value: " + playerHand.getValue());
+            System.out.println("Player's Hand Value: " + playerHand.getValue()); // Display the value of the player's hand
 
-            System.out.println("\nDealer's Hand:");
+            System.out.println("\nDealer's Hand:"); // Display the dealer's hand
             dealerHand.display();
 
             // Player's turn
-            boolean playerTurn = true;
+            boolean playerTurn = true; // Controls if its the player's turn or not
             while (playerTurn) {
                 Scanner scanner = new Scanner(System.in);
-                System.out.print("\nDo you want to hit or pass? (h/p): ");
-                String choice = scanner.nextLine().trim().toLowerCase();
+                System.out.print("\nDo you want to hit or pass? (h/p): "); // Prompt the player for their choice
+                String choice = scanner.nextLine().trim().toLowerCase(); // Read the player's choice and convert it to lowercase and removes blank space
 
                 if (choice.equals("h")) {
                     // Player hits
-                    playerHand.addCard(deck.drawCard());
-                    System.out.println("\nPlayer's Hand:");
+                    playerHand.addCard(deck.drawCard()); // Draw a card for the player
+                    System.out.println("\nPlayer's Hand:"); // Display the player's hand
                     playerHand.display();
-                    System.out.println("Player's Hand Value: " + playerHand.getValue());
+                    System.out.println("Player's Hand Value: " + playerHand.getValue()); // Display the value of the player's hand
 
                     if (playerHand.getValue() > 21) {
-                        System.out.println("\nPlayer busts! You lose.");
-                        losses++;
-                        playerTurn = false;
+                        System.out.println("\nPlayer busts! You lose."); // Player's hand value exceeds 21
+                        losses++; // Add the number of losses
+                        playerTurn = false; // End the player's turn
                     } else if (playerHand.isBlackjack()) {
-                        System.out.println("\nPlayer has Blackjack! You win.");
-                        wins++;
-                        playerTurn = false;
+                        System.out.println("\nPlayer has Blackjack! You win."); // Player has a blackjack (hand value of 21)
+                        wins++; // Add the number of wins
+                        playerTurn = false; // End the player's turn
                     }
                 } else if (choice.equals("p")) {
                     // Player passes
-                    System.out.println("\nPlayer passes.");
-                    playerTurn = false;
+                    System.out.println("\nPlayer passes."); // Player chooses to stop receiving cards
+                    playerTurn = false; // End the player's turn
                 } else {
-                    System.out.println("Invalid choice. Please try again.");
+                    System.out.println("Invalid choice. Please try again."); // Player entered an invalid choice
                 }
             }
 
@@ -75,31 +75,34 @@ public class BlackjackGame {
             int dealerValue = dealerHand.getValue();
 
             if (playerValue > 21) {
-                System.out.println("\nPlayer busts! You lose.");
-                
+                System.out.println("\nPlayer busts! You lose."); // Player's hand value exceeds 21
+                // notice that there is no add of losses here because if you hit (which you must do to get 
+                // above your current number), you will already incur a loss. Therefore if a loss is here as well,
+                // you would incur two losses.
             } else if (dealerValue > 21) {
-                System.out.println("\nDealer busts! You win.");
-                wins++;
+                System.out.println("\nDealer busts! You win."); // Dealer's hand value exceeds 21
+                wins++; // Add the number of wins
             } else if (playerValue == dealerValue) {
-                System.out.println("\nIt's a tie.");
+                System.out.println("\nIt's a tie."); // Player's hand value is equal to the dealer's hand value
+                       // notice no add of wins or losses here because of tie!
             } else if (playerValue > dealerValue) {
-                System.out.println("\nYou win!");
-                wins++;
+                System.out.println("\nYou win!"); // Player's hand value is greater than the dealer's hand value
+                wins++; // Add the number of wins
             } else {
-                System.out.println("\nYou lose.");
-                losses++;
+                System.out.println("\nYou lose."); // Player's hand value is less than the dealer's hand value
+                losses++; // Add the number of losses
             }
 
             // Display scoreboard
             System.out.println("\nScoreboard:");
             System.out.println("Wins: " + wins);
-            System.out.println("Losses: " + losses);
+            System.out.println("Losses: " + losses); 
 
             // Ask if the player wants to play again
             Scanner scanner = new Scanner(System.in);
             System.out.print("\nDo you want to play again? (y/n): ");
             String playAgainChoice = scanner.nextLine().trim().toLowerCase();
-            playAgain = playAgainChoice.equals("y");
+            playAgain = playAgainChoice.equals("y"); // Set based on the player's choice
             System.out.println();
         }
     }
